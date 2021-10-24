ed91e233acc6fbd70ed9575b7b8da6f7bc6d134d
Derek Adams <juugcatm@gmail.com>
2021-10-23 19:24:29 -0700

    Implemented controls matrix. Need to find a way to elide a 0-size controls matrix going forward.

--- lib/kalman_filter.h
+++ lib/kalman_filter.h
@@ -11,19 +11,23 @@ namespace kalman {
   public:
     using StatesVector = Eigen::Matrix<T, Traits::N_states, 1>;
     using ProcessMatrix = Eigen::Matrix<T, Traits::N_states, Traits::N_states>;
+    using ControlsVector = Eigen::Matrix<T, Traits::N_controls, 1>;
+    using ControlsMatrix = Eigen::Matrix<T, Traits::N_states, Traits::N_controls>;
     
     KalmanFilter() {}
     
     void update (double dt) {
       // Perform matrix updates based on dt.
       update_A(dt);
-
+      update_B(dt);
+      
       // Calculate new estimate with the process model.
-      estimate_ = A_ * estimate_;
+      estimate_ = (A_ * estimate_) + (B_ * controls_);
     }
 
-    void initialize (StatesVector initial_estimate) {
+    void initialize (StatesVector initial_estimate, ControlsVector control_params) {
       estimate_ = std::move(initial_estimate);
+      controls_ = std::move(control_params);
     }
 
     const StatesVector estimate () const {
@@ -35,14 +39,28 @@ namespace kalman {
       A_(0, 1) = dt;
     }
 
+    void update_B (double dt) {
+      B_(0, 0) = 0.5 * pow(dt, 2);
+      B_(1, 0) = dt;
+    }
+    
     // The current estimate of the states in the system.
     // Initialization of this class will establish this
     // value during construction.
     StatesVector estimate_ = StatesVector::Zero();
 
+    // The control factors for the model.
+    // Additional factors that are constant throughout
+    // the process and vary with time.
+    ControlsVector controls_ = ControlsVector::Zero();
+    
     // The process matrix. Determines how the dt update is
     // applied to an open-loop process.
     ProcessMatrix A_ = ProcessMatrix::Identity();
+
+    // The controls matrix. Determines how the dt update
+    // is applied to the process with the control values.
+    ControlsMatrix B_ = ControlsMatrix::Zero();
     
   };
   
--- tests/falling_object.cpp
+++ tests/falling_object.cpp
@@ -5,37 +5,40 @@
 
 namespace kalman {
 
-  class RisingFluidTraits {
+  class FallingObjectTraits {
   public:
     static constexpr std::size_t N_states = 2;
     static constexpr std::size_t N_controls = 1;
     static constexpr std::size_t N_meas = 0;
   };
   
-  class KalmanFilterRisingFluidFixture
+  class KalmanFilterFallingObjectFixture
     : public ::testing::Test {
   public:
-    using FilterType = KalmanFilter<double, RisingFluidTraits>;
+    using FilterType = KalmanFilter<double, FallingObjectTraits>;
     using StatesVector = FilterType::StatesVector;
     using ControlsVector = FilterType::ControlsVector;
+    
     FilterType filter_;
   };
   
-  TEST_F(KalmanFilterRisingFluidFixture, works) {
+  TEST_F(KalmanFilterFallingObjectFixture, works) {
     StatesVector initial_state;
     initial_state[0] = 0;    // 0 m of position
-    initial_state[1] = 1.0;  // 1 m/s velocity
+    initial_state[1] = 0;  // 1 m/s velocity
 
-    ControlsVector control_params = ControlsVector::Zero();
-    filter_.initialize(initial_state, control_params);
+    ControlsVector controls;
+    controls[0] = -9.8;     // 1 g of gravity
+    
+    filter_.initialize(initial_state, controls);
 
     filter_.update(0.1);
-    EXPECT_EQ(filter_.estimate()[0], 0.1);
-    EXPECT_EQ(filter_.estimate()[1], 1.0);
+    EXPECT_DOUBLE_EQ(filter_.estimate()[0], -0.049);
+    EXPECT_DOUBLE_EQ(filter_.estimate()[1], -0.98);
 
     filter_.update(1.0);
-    EXPECT_EQ(filter_.estimate()[0], 1.1);
-    EXPECT_EQ(filter_.estimate()[1], 1.0);
+    EXPECT_DOUBLE_EQ(filter_.estimate()[0], -5.929);
+    EXPECT_DOUBLE_EQ(filter_.estimate()[1], -10.78);
   }
  
 }
